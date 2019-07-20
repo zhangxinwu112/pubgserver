393d5f568d9864e38a21b3ee78366005026d1dd0
zhangxinwu112 <zhangxinwu112@163.com>
2019-07-20 11:59:24 +0800

    commit

--- server/server/cs.sql
+++ server/server/cs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2019-07-15 07:46:04
+Date: 2019-07-20 11:59:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,33 +20,38 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `code`;
 CREATE TABLE `code` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `type` smallint(6) DEFAULT NULL,
+  `type` smallint(6) DEFAULT NULL COMMENT '0为次数，1为日期',
   `count` smallint(6) DEFAULT NULL,
-  `expiretime` time DEFAULT NULL,
+  `expiretime` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of code
 -- ----------------------------
+INSERT INTO `code` VALUES ('1', '00001', '0', '-1', '2019-07-18');
+INSERT INTO `code` VALUES ('2', '00002', '1', null, '2019-07-21');
+INSERT INTO `code` VALUES ('3', '00002', '0', '3', null);
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `nick` varchar(255) DEFAULT NULL,
   `telephone` varchar(255) DEFAULT NULL,
-  `sex` varchar(255) DEFAULT NULL,
+  `sex` smallint(2) DEFAULT NULL,
   `note` varchar(1000) DEFAULT NULL,
-  `createdate` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `createdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', '123456', null, null, null, null, null);

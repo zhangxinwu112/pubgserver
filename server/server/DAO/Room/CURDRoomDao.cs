3966baf96dadfafeb21eec48f18cbd49ef7c7893
zhangxinwu112 <zhangxinwu112@163.com>
2020-02-13 22:33:28 +0800

    s

---
+++ server/server/DAO/Room/CURDRoomDao.cs
@@ -0,0 +1,146 @@
+﻿using server.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using server.Tool;
+using MySql.Data.MySqlClient;
+
+namespace server.DAO
+{
+    /// <summary>
+    /// 创建删除修改
+    /// </summary>
+    public class CURDRoomDao : RoomDao
+    {
+        ILog Logger = log4net.LogManager.GetLogger("server.DAO.CURDRoomDao");
+        private readonly int maxNum = 50;
+        public void CreateEditRoom(PubgSession session, string body,string grounpId, string roomId, 
+            string roomName,string checkCode,string userId)
+        {
+            Logger.InfoFormat("创建编辑房间：{0},{1},{2},{3}", grounpId, roomId, roomName, checkCode);
+            
+            if(roomId.Equals("-1"))
+            {
+                string sql = "insert into room(grounpId,name,checkCode,userId) " +
+                   "values('" + grounpId + "','" + roomName + "','" + checkCode + "','" + userId + "')";
+                MySqlExecuteTools.GetAddID(sql);
+            }
+            DataResult dataResult = new DataResult();
+            dataResult.result = 0;
+            session.Send(GetSendData(dataResult, body));
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="body"></param>
+        /// <param name="id">roomid</param>
+        /// <param name="userId">用户id</param>
+        public void ExitRoom(PubgSession session, string body, string grounpId, string userId)
+        {
+            string sql = "select * from room_user where user_id = @user_id";
+            List<Room_User> grounp_UserList = MySqlExecuteTools.GetObjectResult<Room_User>(sql,
+               new MySqlParameter[] { new MySqlParameter("@user_id", userId) });
+
+            grounp_UserList.ForEach((item) => {
+
+                sql = "delete from room_user  where id = @id";
+                MySqlExecuteTools.GetCountResult(sql, new MySqlParameter[] { new MySqlParameter("@id", item.id) });
+
+            });
+
+            DataResult dataResult = new DataResult();
+            dataResult.result = 0;
+            //查询能否删除
+            session.Send(GetSendData(dataResult, body));
+            GetRoomUserData();
+        }
+
+        /// <summary>
+        /// 查询能否进入房间
+        /// </summary>
+        public void SearchEnterRoomState(PubgSession session, string body, string userId)
+        {
+            DataResult dataResult = new DataResult();
+
+            string sql = "select * from room_user where user_id = @user_id";
+            List<Room_User> grounp_UserList = MySqlExecuteTools.GetObjectResult<Room_User>(sql,
+               new MySqlParameter[] { new MySqlParameter("@user_id", userId) });
+
+            dataResult.result = 0;
+            //没有加入房间
+            if (grounp_UserList.Count==0)
+            {
+                dataResult.data = false;
+            }
+
+            if(grounp_UserList.Count==1)
+            {
+                //int grounp_id = grounp_UserList[0].room_id;
+
+                //sql = "select * from room_user where room_id = @room_id";
+                //List<Room_User> _grounp_UserList = MySqlExecuteTools.GetObjectResult<Room_User>(sql,
+                //   new MySqlParameter[] { new MySqlParameter("@room_id", grounp_id) });
+                //if(_grounp_UserList.Count< maxNum)
+                //{
+                //    dataResult.data = false;
+                //}
+                //else
+                //{
+                 dataResult.data = true;
+               // }
+            }
+
+            session.Send(GetSendData(dataResult, body));
+        }
+
+        public void CheckEnterButton(PubgSession session, string body, string checkCode, string userId)
+        {
+            DataResult dataResult = new DataResult();
+            string sql = "select * from room_user where user_id = @user_id";
+            List<Room_User> grounp_UserList = MySqlExecuteTools.GetObjectResult<Room_User>(sql,
+              new MySqlParameter[] { new MySqlParameter("@user_id", userId) });
+
+            if(grounp_UserList.Count==1)
+            {
+                //sql = "select * from room where id = @id  and checkCode = @checkCode";
+                //List<Room> grounps = MySqlExecuteTools.GetObjectResult<Room>(sql,
+                //    new MySqlParameter[] { new MySqlParameter("@id", grounp_UserList[0].room_id), new MySqlParameter("@checkCode", checkCode) });
+                //if(grounps.Count==1)
+                //{
+                dataResult.result = 0;
+                session.Send(GetSendData(dataResult, body));
+                return;
+                //}
+            }
+            dataResult.result = 1;
+            session.Send(GetSendData(dataResult, body));
+        }
+
+        /// <summary>
+        /// 获取最新的grounp_user信息进行grounp信息
+        /// </summary>
+        public void GetRoomUserData()
+        {
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("data", SearchAllRoomUser());
+            EventMgr.Instance.SendEvent(EventName.UPATE_ROOM_USER, dic);
+        }
+
+        public void GetAllRoom()
+        {
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("data", SearchAllRoom());
+            EventMgr.Instance.SendEvent(EventName.ALL_ROOM_DATA, dic);
+        }
+    }
+
+
+    
+
+
+}
--- server/server/cs.sql
+++ server/server/cs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2020-02-11 09:35:30
+Date: 2020-02-13 22:33:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `code` (
   `ctype` smallint(6) DEFAULT NULL COMMENT '0为次数，1为日期',
   `count` smallint(6) DEFAULT NULL,
   `expiretime` bigint(15) NOT NULL DEFAULT '0',
-  `userType` smallint(2) DEFAULT '0',
+  `userType` smallint(2) DEFAULT '0' COMMENT '管理员和玩家分开',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
@@ -34,7 +34,7 @@ CREATE TABLE `code` (
 -- ----------------------------
 INSERT INTO `code` VALUES ('1', '000001', '0', '-1', '0', '0');
 INSERT INTO `code` VALUES ('2', '000002', '1', '0', '1563960333', '0');
-INSERT INTO `code` VALUES ('3', '000003', '0', '15', '0', '0');
+INSERT INTO `code` VALUES ('3', '000003', '0', '15', '0', '1');
 
 -- ----------------------------
 -- Table structure for grounp
@@ -46,7 +46,7 @@ CREATE TABLE `grounp` (
   `area` varchar(10) DEFAULT '陕西地区',
   `userId` int(11) DEFAULT NULL,
   `runState` smallint(2) DEFAULT '-1',
-  `playerTime` smallint(2) DEFAULT '0',
+  `playerTime` smallint(2) DEFAULT '60',
   `fenceLon` double(12,6) DEFAULT '-1.000000',
   `fenceLat` double(12,6) DEFAULT '-1.000000',
   `fenceRadius` int(10) DEFAULT '2000',
@@ -54,12 +54,13 @@ CREATE TABLE `grounp` (
   `checkCode` varchar(10) DEFAULT '123456',
   `roomCount` int(5) DEFAULT '10',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=116 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of grounp
 -- ----------------------------
-INSERT INTO `grounp` VALUES ('115', '管理', null, '30', '-1', '0', '-1.000000', '-1.000000', '2000', '2000', '123456', '10');
+INSERT INTO `grounp` VALUES ('116', '管六安22', '陕西地区', '31', '-1', '25', '-1.000000', '-1.000000', '2000', '2000', '25896', '10');
+INSERT INTO `grounp` VALUES ('117', '二管家', '陕西地区', '33', '-1', '60', '-1.000000', '-1.000000', '2000', '2000', '123456', '10');
 
 -- ----------------------------
 -- Table structure for life
@@ -72,11 +73,12 @@ CREATE TABLE `life` (
   `fightScore` smallint(3) DEFAULT '35' COMMENT '战绩',
   `userId` int(11) DEFAULT '-1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of life
 -- ----------------------------
+INSERT INTO `life` VALUES ('1', '80', '50', '35', '32');
 
 -- ----------------------------
 -- Table structure for machine
@@ -88,12 +90,64 @@ CREATE TABLE `machine` (
   `deviceUniqueIdentifier` varchar(150) DEFAULT NULL,
   `plat` varchar(20) DEFAULT NULL,
   `system` varchar(150) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of machine
 -- ----------------------------
+INSERT INTO `machine` VALUES ('1', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('2', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('3', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', null);
+INSERT INTO `machine` VALUES ('4', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('5', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('6', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('7', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('8', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('9', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('10', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('11', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('12', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('13', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('14', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('15', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('16', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('17', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('18', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('19', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('20', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('21', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('22', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('23', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('24', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('25', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '33');
+INSERT INTO `machine` VALUES ('26', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '33');
+INSERT INTO `machine` VALUES ('27', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('28', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('29', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('30', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('31', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('32', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('33', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('34', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('35', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('36', '3', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '31');
+INSERT INTO `machine` VALUES ('37', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('38', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('39', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('40', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('41', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('42', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('43', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('44', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('45', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('46', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('47', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('48', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('49', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('50', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
+INSERT INTO `machine` VALUES ('51', '1', 'be367d090e0f13218448182032916d36525aeba0', 'Desktop', 'Windows 10  (10.0.0) 64bit', '32');
 
 -- ----------------------------
 -- Table structure for room
@@ -102,17 +156,17 @@ DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `grounpId` int(11) DEFAULT NULL,
-  `code` smallint(4) DEFAULT NULL,
   `name` varchar(10) DEFAULT NULL,
   `checkCode` varchar(10) DEFAULT NULL,
   `userId` int(10) DEFAULT '-1',
   `runState` smallint(2) DEFAULT '-1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=512 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=513 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of room
 -- ----------------------------
+INSERT INTO `room` VALUES ('512', '117', '344', '44', '32', '-1');
 
 -- ----------------------------
 -- Table structure for room_user
@@ -142,9 +196,11 @@ CREATE TABLE `user` (
   `image` varchar(255) DEFAULT '',
   `type` smallint(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('30', '17391767973', '123456', '管理', 'image1', '1');
+INSERT INTO `user` VALUES ('31', '17391767973', '123456', '管六安', 'image1', '1');
+INSERT INTO `user` VALUES ('32', '17391767972', '123456', '天涯', 'image1', '0');
+INSERT INTO `user` VALUES ('33', '17391767974', '123456', '二管家', 'image1', '1');
